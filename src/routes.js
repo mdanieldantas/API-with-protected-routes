7e6fae712738a9a26c652377854c11fead16bdbb
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-02-17 16:56:38 -0300

    adiciona rota de login ao controlador de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authController = require("./controllers/auth-controller");
 
-
+// criando um roteador express
 const router = express.Router();
 
-//
+// rota register do authController
 router.post("/auth/register", authController.register);
+router.post("/auth/register"),authController.login;
 
 module.exports = router;
