25554bd110471d0e2861328ae49d16c4a541511f
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-02-20 16:05:42 -0300

    adiciona método para exibir usuário por ID no controlador de usuários

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -10,7 +10,13 @@ module.exports = {
     },
 
     // GET /users/:id
-
+show: (req, res) => {
+    // pego a id dos parametros da requisição
+    const {id} = req.params;
+    // crio uma constante user e pego o usuario pelo id usando o metodo findById e passando o id da requisição como parametro
+    const user = users.findById(id)
+    if(!user) return res.status(404).json({message:"User not found"})
+}
 
     // POST /users
 
